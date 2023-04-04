0f5a00de033f36df201cc4f07dd6d41158512f8e
Viktoriya Morozova <117348526+vickamorozova@users.noreply.github.com>
2023-04-04 17:26:53 +0800

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,16 +1,13 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
-
 
 chat_id = 357282961 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    n = len(x)
-    b = np.sqrt(2) / 2
-    m = x.mean()
-    s = np.abs(x - m).mean() / b
-    a = 1 / (s ** 2)
-    acceleration = laplace.fit(x, scale=1/a)[1]
-    return acceleration
+    # вычисляем параметр масштаба распределения Лапласа
+    b = np.mean(np.abs(x - np.mean(x)))
+    # вычисляем коэффициент ускорения
+    path_mean = np.mean(x)
+    a = 2 * path_mean / (97**2)
+    return a
