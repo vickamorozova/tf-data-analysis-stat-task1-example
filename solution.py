e680803c59503bb2921c38b01ea5d294569fa250
Viktoriya Morozova <117348526+vickamorozova@users.noreply.github.com>
2023-04-04 17:02:10 +0800

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import numpy as np
 
+from scipy.stats import norm
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+
+chat_id = 357282961 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    n = len(x)
+    b = np.sqrt(2) / 2
+    m = x.mean()
+    s = np.abs(x - m).mean() / b
+    a = 1 / (s ** 2)
+    acceleration = laplace.fit(x, scale=1/a)[1]
+    return acceleration
